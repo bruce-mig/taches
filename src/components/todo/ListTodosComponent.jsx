1ef662db1584a6209a0af7d04fb94b79fccd8942
bruce-mig <brucetmig@icloud.com>
2024-07-01 20:06:46 +0200

    redirect to todo component on update click

--- src/components/todo/ListTodosComponent.jsx
+++ src/components/todo/ListTodosComponent.jsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react"
-import { deleteTodoApi, retrieveAllUserTodosApi } from "./api/TodoApiService"
+import { deleteTodoApi, retrieveAllUserTodosApi, updateTodoApi } from "./api/TodoApiService"
 import { useAuth } from "./security/AuthContext"
+import { useNavigate } from "react-router-dom"
 
 function ListTodosComponent() {
     const authContext = useAuth()
     const username = authContext.username
 
+    const navigate = useNavigate()
     const [todos, setTodos] = useState([])
     const [message, setMessage] = useState(null)
 
@@ -34,6 +36,10 @@ function ListTodosComponent() {
             .catch((error) => console.log(error))
             .finally(() => console.log('cleanup'))
     }
+
+    function updateTodo(id) {
+        navigate(`/todos/${id}`)
+    }
     return (
         <div className='container'>
             <h1>Liste de Taches!</h1>
@@ -46,6 +52,7 @@ function ListTodosComponent() {
                             <th>Description</th>
                             <th>Done</th>
                             <th>Target Date</th>
+                            <th>Update</th>
                             <th>Delete</th>
                         </tr>
                     </thead>
@@ -57,6 +64,9 @@ function ListTodosComponent() {
                                         <td>{todo.description}</td>
                                         <td>{todo.done.toString()}</td>
                                         <td>{todo.targetDate.toString()}</td>
+                                        <td><button className="btn btn-success"
+                                            onClick={() => updateTodo(todo.id)}
+                                        >Update</button></td>
                                         <td><button className="btn btn-warning"
                                             onClick={() => deleteTodo(todo.id)}
                                         >Delete</button></td>
