b37fafc70ec694b59161d1939394495925794422
bruce-mig <brucetmig@icloud.com>
2024-07-01 09:31:50 +0200

    invoke get request

--- src/components/todo/WelcomeComponent.jsx
+++ src/components/todo/WelcomeComponent.jsx
@@ -1,9 +1,24 @@
 import { useParams, Link } from 'react-router-dom'
+import axios from 'axios'
+import { useState } from 'react'
 
 
 function WelcomeComponent() {
 
     const { username } = useParams()
+
+    const [message, setMessage] = useState(null)
+
+    function callHelloWorldRESTAPI() {
+        console.log('called')
+        axios.get('http://localhost:8080/hello-world-bean')
+            .then((response) => {
+                console.log(response)
+                setMessage(response.data.message)
+            })
+            .catch((error) => console.log(error))
+            .finally(() => console.log('cleanup'))
+    }
 
     return (
         <div className="WelcomeComponent">
@@ -11,6 +26,11 @@ function WelcomeComponent() {
             <div>
                 Manage your todos - <Link to="/todos">Go here</Link>.
             </div>
+            <div>
+                <button className='btn btn-success m-5' onClick={callHelloWorldRESTAPI}
+                >Call Hello World</button>
+            </div>
+            <div className='text-info'>{message}</div>
         </div>
     )
 }
